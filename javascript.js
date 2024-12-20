507cc30fcadb3a1b71bf3fd05974c17ff4c8884a
felix.hong <felix.hong0321@gmail.com>
2024-12-19 17:18:15 -0800

    flex box menu

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-const mainBody = document.querySelector("#main-container");
+const mainBody = document.querySelector("#grid-container");
 
-const ROW_SIZE = 20;
-const COL_SIZE = 20;
+let gridSize_n = 20;
 
-function generateGrid(row_size, col_size) {
-    for(let i = 0; i < col_size; i+=1) {
+function generateGrid(gridSize_n) {
+    for(let i = 0; i < gridSize_n; i+=1) {
         const colDiv = document.createElement("div");
         colDiv.classList.add("col-grid-container");
         mainBody.appendChild(colDiv);
 
-        for(let j = 0; j < row_size; j+=1) {
+        for(let j = 0; j < gridSize_n; j+=1) {
             const newDiv = document.createElement("div");
             newDiv.setAttribute("style", "border: 1px solid black; height : 25px; width : 25px;");
+            newDiv.classList.add("row-grid-container");
             newDiv.addEventListener("mouseover",  markGrid);
             colDiv.appendChild(newDiv);
         }
@@ -20,7 +20,7 @@ function generateGrid(row_size, col_size) {
     return 0;
 };
 
-generateGrid(ROW_SIZE,COL_SIZE);
+generateGrid(gridSize_n);
 
 // Track mouse button state
 let mouseButtonStatus = 0; // Track mouse Satus
@@ -30,7 +30,10 @@ document.addEventListener("mouseup" , handleMouseUp );
 
 function handleMouseDown (event) {
     mouseButtonStatus = 1;
-    event.preventDefault(); // prevent dragging of elements
+    if(event.target.classList.contains("row-grid-container") 
+        || event.target.classList.contains("col-grid-container")) {
+            event.preventDefault(); // prevent dragging of grid squares
+    }
 }
 
 function handleMouseUp (event) {
@@ -41,6 +44,7 @@ function markGrid(event) {
     if (mouseButtonStatus) event.target.style.background = "black";
 }
 
+// Set function for reset button
 const resetButton = document.querySelector("#grid-reset-button");
 resetButton.addEventListener("click", resetGrid);
 
@@ -48,5 +52,18 @@ function resetGrid(event) {
     while(mainBody.hasChildNodes()) {
         mainBody.removeChild(mainBody.firstChild);
     }
-    generateGrid(ROW_SIZE,COL_SIZE);
+    generateGrid(gridSize_n);
+}
+
+// handleSlider
+const gridSizeSlider = document.querySelector("#set-grid-size");
+const gridSize_display = document.querySelector("#display-grid-size");
+
+gridSizeSlider.addEventListener("input", handleSlider);
+gridSizeSlider.addEventListener("change", resetGrid);
+
+function handleSlider(event) {
+    gridSize_n = event.target.value;
+    gridSize_display.textContent = gridSize_n;
+
 }
