d51628814020c209461ce4431f28e56c3f472b00
felix.hong <felix.hong0321@gmail.com>
2024-12-19 19:08:35 -0800

    final javascript design

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const mainBody = document.querySelector("#grid-container");
 
+// generate grid
 let gridSize_n = 20;
 
 function generateGrid(gridSize_n) {
@@ -10,12 +11,12 @@ function generateGrid(gridSize_n) {
 
         for(let j = 0; j < gridSize_n; j+=1) {
             const newDiv = document.createElement("div");
-            newDiv.setAttribute("style", "border: 1px solid black; height : 25px; width : 25px;");
+            //newDiv.setAttribute("style", "border: 1px solid black; height : 25px; width : 25px;");
+            newDiv.setAttribute("style", "border: 1px solid hsl(0deg 0% 40%);");
             newDiv.classList.add("row-grid-container");
-            newDiv.addEventListener("mouseover",  markGrid);
+            newDiv.addEventListener("mousemove",  markGrid);
             colDiv.appendChild(newDiv);
         }
-    
     }
     return 0;
 };
@@ -34,36 +35,66 @@ function handleMouseDown (event) {
         || event.target.classList.contains("col-grid-container")) {
             event.preventDefault(); // prevent dragging of grid squares
     }
-}
+};
 
 function handleMouseUp (event) {
     mouseButtonStatus = 0;
-}
+};
 
+// handle the gridColoring
 function markGrid(event) {
-    if (mouseButtonStatus) event.target.style.background = "black";
-}
+    if (mouseButtonStatus) event.target.style.background = gridColor;
+};
 
 // Set function for reset button
 const resetButton = document.querySelector("#grid-reset-button");
+
 resetButton.addEventListener("click", resetGrid);
 
 function resetGrid(event) { 
-    while(mainBody.hasChildNodes()) {
-        mainBody.removeChild(mainBody.firstChild);
-    }
-    generateGrid(gridSize_n);
-}
+    const gridSquareALl = document.querySelectorAll(".row-grid-container")
+    gridSquareALl.forEach(ele => ele.style.background = "white");
+};
 
 // handleSlider
 const gridSizeSlider = document.querySelector("#set-grid-size");
 const gridSize_display = document.querySelector("#display-grid-size");
 
 gridSizeSlider.addEventListener("input", handleSlider);
-gridSizeSlider.addEventListener("change", resetGrid);
+gridSizeSlider.addEventListener("change", handleSliderSet);
 
 function handleSlider(event) {
     gridSize_n = event.target.value;
-    gridSize_display.textContent = gridSize_n;
+    gridSize_display.value = gridSize_n;
 
-}
+};
+
+function handleSliderSet(event) {
+    while(mainBody.hasChildNodes()) {
+        mainBody.removeChild(mainBody.firstChild);
+    }
+    generateGrid(gridSize_n);
+};
+
+// handle valueChangeGridSize
+gridSize_display.addEventListener("change", handleGridSizeInput)
+function handleGridSizeInput(event) {
+    gridSize_n = gridSize_display.value;
+    gridSizeSlider.value = gridSize_n;
+
+    while(mainBody.hasChildNodes()) {
+        mainBody.removeChild(mainBody.firstChild);
+    }
+    generateGrid(gridSize_n);
+};
+
+// track colorPicker
+let gridColor = "#000000"
+const colorPicker = document.querySelector("#grid-color");
+
+colorPicker.addEventListener("input", handleColorPick);
+
+function handleColorPick (event) {
+    gridColor = event.target.value;
+    console.log(gridColor);
+};
